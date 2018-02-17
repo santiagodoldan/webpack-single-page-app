f0b479c6ac12201997b3a5e580435c8c55df7b68
Santiago Doldan <santiagodolte@gmail.com>
2018-02-17 19:36:35 -0300

    Create fake data if no bikers available

--- app/index.js
+++ app/index.js
@@ -2,12 +2,48 @@ require("font-awesome-webpack")
 
 // Configure storage adapter
 
-const { LocalStorageAdapter } = require("./lib/storages")
+const { RideInGroups } = require("./interfaces")
 const { Storage } = require("./lib/storage")
+const { LocalStorageAdapter } = require("./lib/storages")
+const { Biker } = require("./models/biker")
 
-const storage = Storage.getInstance()
+Storage.getInstance().adapter = new LocalStorageAdapter()
 
-storage.adapter = new LocalStorageAdapter()
+/**
+ * Create fake data if no bikers available
+ */
+if (!Biker.findAll().length) {
+  const biker1 = new Biker({
+    name: "James Isaac Neutro",
+    email: "neutron@example.com",
+    city: "City",
+    rideInGroups: RideInGroups.Always,
+    frequency: [0, 1, 2, 3, 4, 5, 6],
+    createdAt: new Date(),
+  })
+
+  const biker2 = new Biker({
+    name: "Carl Wheezer",
+    email: "carl@example.com",
+    city: "City",
+    rideInGroups: RideInGroups.Sometimes,
+    frequency: [1, 2, 3, 4, 5],
+    createdAt: new Date(),
+  })
+
+  const biker3 = new Biker({
+    name: "Cindy Vortex",
+    email: "cindyvortex@example.com",
+    city: "City",
+    rideInGroups: RideInGroups.Never,
+    frequency: [1, 3, 5],
+    createdAt: new Date(),
+  })
+
+  biker1.save()
+  biker2.save()
+  biker3.save()
+}
 
 // Initialize all components
 
